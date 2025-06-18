b287360428cbb0200d3d1e5378a785ba88ce693b
MCacciavillani <mario_ac_max@hotmail.com>
2025-06-17 23:49:40 -0300

    Swagger implementado

--- src/modules/workerServices/dtos/service.dto.ts
+++ src/modules/workerServices/dtos/service.dto.ts
@@ -1,19 +1,31 @@
 import { IsNotEmpty, IsString, IsUUID } from 'class-validator';
 
 export class ServiceDto {
+  /**
+  @example ASDGRgrdgdth5461321d
+  */
   @IsNotEmpty()
   @IsString()
   @IsUUID()
   worker_id: string;
 
+  /**
+  @example Categoria1
+  */
   @IsNotEmpty()
   @IsString()
   category: string;
 
+  /**
+  @example Servicio1
+  */
   @IsNotEmpty()
   @IsString()
   title: string;
 
+  /**
+  @example Este-es-un-servicio-de-prueba
+  */
   @IsNotEmpty()
   @IsString()
   description: string;
