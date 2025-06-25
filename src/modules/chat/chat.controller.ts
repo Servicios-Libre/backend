024dfae430c520a115a07d9d3c07b4f803a392ef
MCacciavillani <mario_ac_max@hotmail.com>
2025-06-24 23:29:05 -0300

    cambios recepcion de datos

--- src/modules/chat/chat.controller.ts
+++ src/modules/chat/chat.controller.ts
@@ -25,9 +25,7 @@ export class ChatController {
 
   @UseGuards(JwtAuthGuard)
   @Post('start')
-  getConversation(
-    @Body() { userID, otherUserID }: { userID: string; otherUserID: string },
-  ) {
+  getConversation(@Body() userID: string, @Body() otherUserID: string) {
     console.log('userID', userID, 'otherUserID', otherUserID);
     // @Query('user1', ParseUUIDPipe) user1: string,
     // @Query('user2', ParseUUIDPipe) user2: string,
