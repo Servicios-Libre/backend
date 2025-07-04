ea77bf14789ffd507e3a7e36834e8309198db431
Martin Tavella <martutavella@gmail.com>
2025-07-04 20:19:54 -0300

    Actualizado el DTO de user para que de errores en español y ser consumidos en front

--- src/modules/users/DTOs/user.dto.ts
+++ src/modules/users/DTOs/user.dto.ts
@@ -11,111 +11,158 @@ import {
   Min,
 } from 'class-validator';
 import { Role } from '../entities/roles.enum';
+import { ApiHideProperty } from '@nestjs/swagger';
 
 export class UserDto {
   /**
   @example example@gmail.com
   */
-  @IsNotEmpty()
+  @IsNotEmpty({
+    message: 'Debe haber un email',
+  })
   @IsEmail()
-  @IsString()
+  @IsString({
+    message: 'El email debe ser un string',
+  })
   email: string;
 
   /**
-  @example Password123
+  @example Contra123@
   */
-  @IsNotEmpty()
-  @IsString()
-  @Length(8, 20)
+  @IsNotEmpty({
+    message: 'Debe haber una contraseña',
+  })
+  @IsString({
+    message: 'La contraseña debe ser un string',
+  })
+  @Length(8, 20, {
+    message: 'La contraseña debe tener entre 8 y 20 caracteres',
+  })
   @Matches(/(?=.*[!@#$%^&*(),.?":{}|<>])/, {
-    message: 'The password must have at least one special char',
+    message: 'La contraseña debe tener al menos un carácter especial',
   })
   @Matches(/(?=.*[A-Z])/, {
-    message: 'The password must have at least one uppercase letter',
+    message: 'La contraseña debe tener al menos una letra mayúscula',
   })
   password: string;
 
   /**
-  @example Password123
+  @example Contra123@
   */
-  @IsNotEmpty()
-  @IsString()
+  @IsNotEmpty({
+    message: 'Debe haber una confirmación de contraseña',
+  })
+  @IsString({
+    message: 'La confirmación de contraseña debe ser un string',
+  })
   confirmPassword: string;
 
   /**
   @example Nombre
   */
-  @IsNotEmpty()
-  @IsString()
-  @MaxLength(20)
+  @IsNotEmpty({
+    message: 'Debe haber un nombre',
+  })
+  @IsString({
+    message: 'El nombre debe ser un string',
+  })
+  @MaxLength(20, {
+    message: 'El nombre debe tener menos de 20 caracteres',
+  })
   @Matches(/^[A-Za-zÁÉÍÓÚáéíóúÑñÜü\s]+$/, {
-    message: 'The name can only contain letters (including accents) and spaces',
+    message: 'El nombre debe tener solo letras y espacios',
   })
   name: string;
 
   /**
   @example 03436569569
   */
-  @IsNotEmpty()
-  @IsString()
-  @MaxLength(20)
+  @IsNotEmpty({
+    message: 'Debe haber un teléfono',
+  })
+  @IsString({
+    message: 'El teléfono debe ser un string',
+  })
+  @MaxLength(20, {
+    message: 'El teléfono debe tener menos de 20 caracteres',
+  })
   phone: string;
 
-  @IsEmpty()
+  @ApiHideProperty()
+  @IsEmpty({
+    message: 'No debe haber un rol',
+  })
   role: Role;
 
-  @IsEmpty()
+  @ApiHideProperty()
+  @IsEmpty({ message: 'No debe haber un premium' })
   premium: boolean;
 
   /**
-  @example Calle-Falsa
+  @example Calle Falsa
   */
-  @IsNotEmpty()
-  @IsString()
-  @MaxLength(20)
+  @IsNotEmpty({
+    message: 'Debe haber una calle',
+  })
+  @IsString({
+    message: 'La calle debe ser un string',
+  })
+  @MaxLength(20, { message: 'La calle debe tener menos de 20 caracteres' })
   @Matches(/^[A-Za-zÁÉÍÓÚáéíóúÑñÜü\s]+$/, {
-    message:
-      'The address can only contain letters (including accents) and spaces',
+    message: 'La calle debe tener solo letras y espacios',
   })
   street: string;
 
   /**
   @example 123
   */
-  @IsNotEmpty()
-  @IsInt()
-  @Min(1)
-  @Max(10000)
+  @IsNotEmpty({
+    message: 'Debe haber un número de casa',
+  })
+  @IsInt({ message: 'El número de casa debe ser un número entero' })
+  @Min(1, { message: 'El número de casa debe ser mayor a 0' })
+  @Max(10000, { message: 'El número de casa debe ser menor a 10000' })
   house_number: number;
 
   /**
-  @example Gran-Ciudad
+  @example Gran Ciudad
   */
-  @IsNotEmpty()
-  @IsString()
-  @MaxLength(20)
+  @IsNotEmpty({
+    message: 'Debe haber una ciudad',
+  })
+  @IsString({
+    message: 'La ciudad debe ser un string',
+  })
+  @MaxLength(20, { message: 'La ciudad debe tener menos de 20 caracteres' })
   @Matches(/^[A-Za-zÁÉÍÓÚáéíóúÑñÜü\s]+$/, {
-    message: 'The city can only contain letters (including accents) and spaces',
+    message: 'La ciudad debe tener solo letras y espacios',
   })
   city: string;
 
   /**
-  @example Gran-Pais
+  @example Gran Provincia
   */
-  @IsNotEmpty()
-  @IsString()
-  @MaxLength(20)
+  @IsNotEmpty({
+    message: 'Debe haber una provincia',
+  })
+  @IsString({
+    message: 'La provincia debe ser un string',
+  })
+  @MaxLength(20, { message: 'La provincia debe tener máximo 20 caracteres' })
   @Matches(/^[A-Za-zÁÉÍÓÚáéíóúÑñÜü\s]+$/, {
-    message:
-      'The state can only contain letters (including accents) and spaces',
+    message: 'La provincia debe tener solo letras y espacios',
   })
   state: string;
 
   /**
   @example E3100
   */
-  @IsNotEmpty()
-  @IsString()
-  @MaxLength(7)
+  @IsNotEmpty({
+    message: 'Debe haber un código postal',
+  })
+  @IsString({
+    message: 'El código postal debe ser un string',
+  })
+  @MaxLength(7, { message: 'El código postal debe tener máximo 7 caracteres' })
   zip_code: string;
 }
